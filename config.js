835b5be836510c5c747267df14e1ff7c4989896c
RAGHUVEER BHATI <raghuveerbhati033@gmail.com>
2026-03-08 18:27:45 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,46 @@
+// Gym ke liye and Library ke liye 
+
+// src/config.js
+
+const config = {
+  // Business Details
+  appName: "Laxmi Library",       // Gym ke liye: "FitLife Gym"
+  branchName: "Bikaner Branch",   // Gym ke liye: "JNV Colony Branch"
+  mainEmoji: "📚",                // Gym ke liye: "🏋️"
+  
+  // User Terminology
+  userType: "Student",            // Gym ke liye: "Member"
+  userTypePlural: "Students",     // Gym ke liye: "Members"
+  
+  // Plan Terminology
+  planLabel: "Plan",              // Gym ke liye: "Package"
+  
+  // Receipt Prefix
+  receiptPrefix: "LX",            // Gym ke liye: "FIT"
+};
+
+export default config;
+
+// GYM ke liye 
+
+// src/config.js
+
+const config = {
+  // Business Details
+  appName: "Body GYM",       // Gym ke liye: "FitLife Gym"
+  branchName: "Bikaner Branch",   // Gym ke liye: "JNV Colony Branch"
+  mainEmoji: "🏋️",                // Gym ke liye: "🏋️"
+  
+  // User Terminology
+  userType: "Member",            // Gym ke liye: "Member"
+  userTypePlural: "Members",     // Gym ke liye: "Members"
+  
+  // Plan Terminology
+  planLabel: "Package",              // Gym ke liye: "Package"
+  
+  // Receipt Prefix
+  receiptPrefix: "FIT",            // Gym ke liye: "FIT"
+};
+
+export default config;
+
